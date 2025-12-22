c3708e97e496cd8b02b6ed77ef3addcce0dab85f
bala-amypo <bala@amypo.in>
2025-12-22 13:30:02 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,27 +1,27 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 import com.example.demo.dto.AuthRequest;
 import com.example.demo.dto.AuthResponse;
 import com.example.demo.dto.UserRegisterDto;
-import com.example.demo.model.User;
 import com.example.demo.service.UserService;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
-@RequiredArgsConstructor
-@Tag(name = "Authentication")
 public class AuthController {
 
-    private final UserService userService;
+    @Autowired
+    private UserService userService;
 
+    // ✅ REGISTER
     @PostMapping("/register")
-    public User register(@RequestBody UserRegisterDto dto) {
+    public AuthResponse register(@RequestBody UserRegisterDto dto) {
         return userService.register(dto);
     }
 
+    // ✅ LOGIN
     @PostMapping("/login")
     public AuthResponse login(@RequestBody AuthRequest request) {
         return userService.login(request);
