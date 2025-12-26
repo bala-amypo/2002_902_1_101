50ddbb46a8f7fbada5451237a8cabec067390495
bala-amypo <bala@amypo.in>
2025-12-26 06:09:08 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/ConsumptionLogServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ConsumptionLogServiceImpl.java
@@ -1,4 +1,29 @@
-@Override
-public List<ConsumptionLog> getByProduct(Long productId) {
-    return List.of();
+package com.example.demo.service.impl;
+
+import com.example.demo.model.ConsumptionLog;
+import com.example.demo.repository.ConsumptionLogRepository;
+import com.example.demo.service.ConsumptionLogService;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ConsumptionLogServiceImpl implements ConsumptionLogService {
+
+    private final ConsumptionLogRepository consumptionLogRepository;
+
+    public ConsumptionLogServiceImpl(ConsumptionLogRepository consumptionLogRepository) {
+        this.consumptionLogRepository = consumptionLogRepository;
+    }
+
+    @Override
+    public ConsumptionLog save(ConsumptionLog log) {
+        return consumptionLogRepository.save(log);
+    }
+
+    @Override
+    public List<ConsumptionLog> getByProduct(Long productId) {
+        return consumptionLogRepository.findByProductId(productId);
+    }
 }
