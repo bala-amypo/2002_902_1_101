7e9662002cc84c88ec8f763a3c0aed7dea9e7c60
bala-amypo <bala@amypo.in>
2025-12-27 09:18:49 +0000

    User commit

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,14 +1,29 @@
+// package com.example.demo.controller;
+
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
+
+// @RestController
+// public class HomeController {
+
+//     @GetMapping("/")
+//     public String home() {
+//         return "Welcome to Inventory Management System";
+//     }
+// }
 package com.example.demo.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HomeController {
+public class RootController {
 
     @GetMapping("/")
-    public String home() {
-        return "Welcome to Inventory Management System";
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public void root() {
+        // intentionally empty
     }
 }
-
