a8e0dbe4480c30f05d862709bf8b69ee6637fdaf
bala-amypo <bala@amypo.in>
2025-12-27 09:45:49 +0000

    User commit

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,16 +1,14 @@
 package com.example.demo.controller;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.http.HttpStatus;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 public class HomeController {
 
-@GetMapping("/")
-@ResponseStatus(HttpStatus.NOT_FOUND)
-
-    public void home() {
+    @GetMapping("/")  // maps the root URL
+    public ResponseEntity<String> home() {
+        return ResponseEntity.status(404).body("not Found");
     }
 }
