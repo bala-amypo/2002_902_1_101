525b81b15d19c2f249ce7608a2d7f218eda650f4
bala-amypo <bala@amypo.in>
2025-12-28 06:01:04 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.AuthRequest;
+import com.example.demo.dto.UserRegisterDto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,12 +10,12 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     @PostMapping("/register")
-        public ResponseEntity<String> register(@RequestBody Object request) {
-                return ResponseEntity.ok("User registered successfully");
-                    }
+    public ResponseEntity<String> register(@RequestBody UserRegisterDto request) {
+        return ResponseEntity.ok("User registered successfully");
+    }
 
-                        @PostMapping("/login")
-                            public ResponseEntity<String> login(@RequestBody Object request) {
-                                    return ResponseEntity.status(401).body("Invalid credentials");
-                                        }
-                                        }
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody AuthRequest request) {
+        return ResponseEntity.status(401).body("Invalid credentials");
+    }
+}
